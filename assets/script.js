abf1dd3c7b1252cd0c2cac76a86a4e9e4b07ae0b
dmmontoto <dmontoto@ucsd.edu>
2023-07-26 17:36:38 -0700

    created switch data function using local storage

--- assets/script.js
+++ assets/script.js
@@ -44,7 +44,7 @@ function getWeatherData(cityName) {
           .then(response => response.json())
           .then(data => {
             var today = dayjs();
-            console.log(data);
+
             const cityId = data.city.id;
             const cityName = data.city.name;
             const fiveDayForecast = data.list;
@@ -169,8 +169,9 @@ function getWeatherData(cityName) {
                 const cityWeatherDataJSON = JSON.stringify(cityWeatherData);
                 console.log(cityId.toString());
                 localStorage.setItem(cityId.toString(), cityWeatherDataJSON);
-                console.log('done');
                 console.log(`City with ID ${cityId} does not exist in localStorage.`);
+                // keep tally
+                tally = tally + 1;
             }
           })
           .catch(error => {
@@ -190,6 +191,107 @@ function checkIfCityExists(cityId) {
 }
 
 
+function switchData(cityId) {
+    const newWeatherDataJSON = localStorage.getItem(cityId.toString());
+    const cityWeatherData = JSON.parse(newWeatherDataJSON);
+    const cityName = cityWeatherData.cityName;
+    const currentWeather = cityWeatherData.currentWeather;
+    const fiveDayForecast = cityWeatherData.fiveDayForecast;
+    var today = dayjs();
+
+     // TODAY
+     var temperature = currentWeather.main.temp;
+     temperature = (1.8 * (temperature - 273) + 32);
+     var date = today.format('(MM/DD/YYYY)');
+     var location = cityName;
+     var wind = currentWeather.wind.speed;
+     wind = (wind / 1609) * 3600;
+     var humidity = currentWeather.main.humidity;
+
+     $('#Temp').text('Temp: ' + temperature.toFixed(2) + ' °F');
+     $('#Location-date').text(location + " " + date);
+     $('#Wind').text('Wind: ' + wind.toFixed(2) + ' mph');
+     $('#Humidity').text('Humidity: ' + humidity + ' %');
+
+     const day1Weather = fiveDayForecast[7];
+     const day2Weather = fiveDayForecast[16];
+     const day3Weather = fiveDayForecast[23];
+     const day4Weather = fiveDayForecast[31];
+     const day5Weather = fiveDayForecast[39];
+
+     // DAY 1 (Tomorrow)
+     var temp1 = day1Weather.main.temp;
+     temp1 = (1.8 * (temp1 - 273) + 32);
+     var day1 = today.add(1, 'day');
+     var date1 = day1.format('(MM/DD/YYYY)');
+     var wind1 = day1Weather.wind.speed
+     wind1 = (wind1 / 1609) * 3600;
+     var humidity1 = day1Weather.main.humidity;
+
+     $('#Temp1').text('Temp: ' + temp1.toFixed(2) + ' °F');
+     $('#Date1').text(date1);
+     $('#Wind1').text('Wind: ' + wind1.toFixed(1) + ' mph');
+     $('#Humidity1').text('Humidity: ' + humidity1 + ' %');
+
+     // DAY 2
+     var temp2 = day2Weather.main.temp;
+     temp2 = (1.8 * (temp2 - 273) + 32);
+     var day2 = today.add(2, 'day');
+     var date2 = day2.format('(MM/DD/YYYY)');
+     var wind2 = day2Weather.wind.speed
+     wind2 = (wind2 / 1609) * 3600;
+     var humidity2 = day2Weather.main.humidity;
+
+     $('#Temp2').text('Temp: ' + temp2.toFixed(2) + ' °F');
+     $('#Date2').text(date2);
+     $('#Wind2').text('Wind: ' + wind2.toFixed(1) + ' mph');
+     $('#Humidity2').text('Humidity: ' + humidity2 + ' %');
+
+     // DAY 3
+     var temp3 = day3Weather.main.temp;
+     temp3 = (1.8 * (temp3 - 273) + 32);
+     var day3 = today.add(3, 'day');
+     var date3 = day3.format('(MM/DD/YYYY)');
+     var wind3 = day3Weather.wind.speed
+     wind3 = (wind3 / 1609) * 3600;
+     var humidity3 = day3Weather.main.humidity;
+
+     $('#Temp3').text('Temp: ' + temp3.toFixed(2) + ' °F');
+     $('#Date3').text(date3);
+     $('#Wind3').text('Wind: ' + wind3.toFixed(1) + ' mph');
+     $('#Humidity3').text('Humidity: ' + humidity3 + ' %');
+
+     // DAY 4
+     var temp4 = day4Weather.main.temp;
+     temp4 = (1.8 * (temp4 - 273) + 32);
+     var day4 = today.add(4, 'day');
+     var date4 = day4.format('(MM/DD/YYYY)');
+     var wind4 = day4Weather.wind.speed
+     wind4 = (wind4 / 1609) * 3600;
+     var humidity4 = day4Weather.main.humidity;
+
+     $('#Temp4').text('Temp: ' + temp4.toFixed(2) + ' °F');
+     $('#Date4').text(date4);
+     $('#Wind4').text('Wind: ' + wind4.toFixed(1) + ' mph');
+     $('#Humidity4').text('Humidity: ' + humidity4 + ' %');
+
+     // DAY 5
+     var temp5 = day5Weather.main.temp;
+     temp5 = (1.8 * (temp5 - 273) + 32);
+     var day5 = today.add(5, 'day');
+     var date5 = day5.format('(MM/DD/YYYY)');
+     var wind5 = day5Weather.wind.speed
+     wind5 = (wind5 / 1609) * 3600;
+     var humidity5 = day5Weather.main.humidity;
+
+     $('#Temp5').text('Temp: ' + temp5.toFixed(2) + ' °F');
+     $('#Date5').text(date5);
+     $('#Wind5').text('Wind: ' + wind5.toFixed(1) + ' mph');
+     $('#Humidity5').text('Humidity: ' + humidity5 + ' %');
+
+}
+
+
 var miami = 'Miami';
 getWeatherData(miami);
 var sacramento = 'Sacramento';
@@ -201,10 +303,26 @@ getWeatherData(sacramento);
 // clear();
 
 $('.searchBtn').on('click', function () {
+    var london = 'London';
+    getWeatherData(london);
+    console.log(london);
+    
     console.log('hello');
-    formSubmitHandler();
+    var city = cityEl.value.trim();
+    var london = 'London';
+    getWeatherData(london);
+    console.log(london);
+  
+    if (city) {
+      getWeatherData(city);
+
+      cityEl.value = '';
+    } else {
+      alert('Please enter a city');
+    }
 })
 
 $('.savedBtn').on('click', function () {
     console.log('bye');
 })
+
